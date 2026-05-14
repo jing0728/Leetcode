1fa99beebc2b56a97820c3bdef45fb75e812917f
jing0728 <98197500+jing0728@users.noreply.github.com>
2026-05-14 10:42:53 -0400

    hashmap

--- 49_Group_Anagrams.py
+++ 49_Group_Anagrams.py
@@ -4,4 +4,6 @@ class Solution:
         for word in strs:
             key=tuple(sorted(word))
             map[key] = map.get(key, []) + [word]
+            # 如果 key 存在，取出已有列表再加上新词
+            # 如果 key 不存在，默认空列表 [] 再加上新词
         return list(map.values())
