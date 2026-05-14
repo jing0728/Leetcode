2cec36741d66535d84f1e38ab072ddd258dd84f6
jing0728 <98197500+jing0728@users.noreply.github.com>
2026-05-14 09:29:34 -0400

    AO

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,4 @@
+#hashmap
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         map = {}  # 值 → 索引
@@ -6,4 +7,13 @@ class Solution:
             if complement in map:
                 return [map[complement], i]
             map[num] = i
+        return []
+    
+#遍历
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)): 
+                if nums[i] + nums[j] == target:
+                    return [i, j]
         return []
